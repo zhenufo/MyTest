c7edc3e341d374e0f3136ba538292b6cca5de5d6
zhenufo <11300720122@fudan.edu.cn>
2017-10-10 17:31:38 +0800

    Create shadowsocksonkey2.sh

---
+++ shadowsocksonkey2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+location=~+
+echo "$location"
+chkconfig iptables off;
+echo "########################欢迎使用shadowsocks搭建到优化一键安装脚本包#########################"
+read -p "即将安装锐速、ss并优化 是否安装 :[y/n]"  syskel
+if [[ "$syskel" = "y" ]] ; then 
+    echo "开始安装锐速" 
+	cd /root
+	wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh;
+	echo "########################优化内核参数#########################"
+	sed -i "s/rsc=\"[0-1]\"/rsc=\"1\"/g" /serverspeeder/etc/config;
+	sed -i "s/gso=\"[0-1]\"/gso=\"1\"/g" /serverspeeder/etc/config;
+	sed -i "s/maxmode=\"[0-1]\"/maxmode=\"1\"/g" /serverspeeder/etc/config;
+	sed -i "s/advinacc=\"[0-1]\"/advinacc=\"1\"/g" /serverspeeder/etc/config;
+	sed -i '$a\* soft nofile 51200' /etc/security/limits.conf;
+	sed -i '$a\* hard nofile 51200' /etc/security/limits.conf;
+	sed -i '$a\ulimit -SHn 51200' /etc/profile;
+	service serverSpeeder restart;
+	cat $location/sysctl >> /etc/sysctl.conf;
+	sysctl -p;
+fi
